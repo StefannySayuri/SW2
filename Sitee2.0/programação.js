73b3cf19c0041065de876d82c9682efd5af08186
Sayuri <125521143+StefannySayuri@users.noreply.github.com>
2025-02-14 18:25:50 -0300

    Add files via upload

---
+++ Sitee2.0/programação.js
@@ -0,0 +1,89 @@
+function mostrarConteudo() {
+    var feijaoTropeiro = document.getElementById("feijaoTropeiro");
+    if (feijaoTropeiro.style.display === "none") {
+        feijaoTropeiro.style.display = "block"; // Mostra o conteúdo
+    } else {
+        feijaoTropeiro.style.display = "none"; // Esconde o conteúdo novamente se clicar de novo
+    }
+}
+
+function mostrarConteudo() {
+    var tutudeFeijao = document.getElementById("tutudeFeijao");
+    if (tutudeFeijao.style.display === "none") {
+        tutudeFeijao.style.display = "block"; // Mostra o conteúdo
+    } else {
+        tutudeFeijao.style.display = "none"; // Esconde o conteúdo novamente se clicar de novo
+    }
+}
+
+function mostrarConteudo() {
+    var FrangoQuiabo = document.getElementById("FrangoQuiabo");
+    if (FrangoQuiabo.style.display === "none") {
+        FrangoQuiabo.style.display = "block"; // Mostra o conteúdo
+    } else {
+        FrangoQuiabo.style.display = "none"; // Esconde o conteúdo novamente se clicar de novo
+    }
+}
+
+function mostrarConteudo() {
+    var vacaAtolada = document.getElementById("vacaAtolada");
+    if (vacaAtolada.style.display === "none") {
+        vacaAtolada.style.display = "block"; // Mostra o conteúdo
+    } else {
+        vacaAtolada = "none"; // Esconde o conteúdo novamente se clicar de novo
+    }
+}
+
+function mostrarConteudo() {
+    var canjiquinha = document.getElementById("canjiquinha");
+    if (fcanjiquinha.style.display === "none") {
+        canjiquinha.style.display = "block"; // Mostra o conteúdo
+    } else {
+        canjiquinha.style.display = "none"; // Esconde o conteúdo novamente se clicar de novo
+    }
+}
+
+function mostrarConteudo() {
+    var CozidoMineiro = document.getElementById("CozidoMineiro");
+    if (CozidoMineiro.style.display === "none") {
+        CozidoMineiro.style.display = "block"; // Mostra o conteúdo
+    } else {
+        CozidoMineiro.style.display = "none"; // Esconde o conteúdo novamente se clicar de novo
+    }
+}
+
+function mostrarConteudo() {
+    var paodeQueijo = document.getElementById("paodeQueijo");
+    if (paodeQueijo.style.display === "none") {
+        paodeQueijo.style.display = "block"; // Mostra o conteúdo
+    } else {
+        paodeQueijo.style.display = "none"; // Esconde o conteúdo novamente se clicar de novo
+    }
+}
+
+function mostrarConteudo() {
+    var biscoitoPolvilho = document.getElementById("biscoitoPolvilho");
+    if (biscoitoPolvilho.style.display === "none") {
+        biscoitoPolvilho.style.display = "block"; // Mostra o conteúdo
+    } else {
+        biscoitoPolvilho.style.display = "none"; // Esconde o conteúdo novamente se clicar de novo
+    }
+}
+
+function mostrarConteudo() {
+    var curau = document.getElementById("curau");
+    if (curau.style.display === "none") {
+        curau.style.display = "block"; // Mostra o conteúdo
+    } else {
+        curau.style.display = "none"; // Esconde o conteúdo novamente se clicar de novo
+    }
+}
+
+function mostrarConteudo() {
+    var ambrosia = document.getElementById("ambrosia");
+    if (ambrosia.style.display === "none") {
+        ambrosia.style.display = "block"; // Mostra o conteúdo
+    } else {
+        ambrosia.style.display = "none"; // Esconde o conteúdo novamente se clicar de novo
+    }
+}
